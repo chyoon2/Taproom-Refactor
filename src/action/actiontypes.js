34b075b5cad6026de2803c587754b3971e701377
Chris Yoon <chy.yoon@gmail.com>
2020-09-21 22:25:55 -0700

    added function and variables into action type and index.js

---
+++ src/action/actiontypes.js
@@ -0,0 +1,3 @@
+export const ADD_TICKET = 'ADD_KEG';
+export const DELETE_TICKET = 'DELETE_KEG';
+export const TOGGLE_FORM = 'TOGGLE_FORM';
---
+++ src/action/index.js
@@ -0,0 +1,17 @@
+import * as c from './ActionTypes';
+
+export const deleteKeg = id => ({
+  type: c.DELETE_KEG,
+  id
+});
+
+export const toggleForm = () => ({
+  type: c.TOGGLE_FORM
+});
+
+
+export const updateTime = (id, formattedWaitTime) => ({
+  type: c.UPDATE_TIME,
+  id: id,
+  formattedWaitTime: formattedWaitTime
+});
